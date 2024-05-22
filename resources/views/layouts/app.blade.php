5d469595fc2a2a4e3f02e9214b25ddb274a808b3
chewei <joseph7492748@gmail.com>
2024-05-22 22:46:59 +0800

    Fixed surprise

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
         <div class="navbar">
             <div class="nav-brand">
                 <a class="nav-link-brand" href="{{ url('/') }}">
-                    <img src="http://localhost:8080/Meal/public/images/logo.svg" alt="logo" height="40px" width="100px" />
+                    <img src="http://localhost:8080/Meal/public/images/logo.svg" alt="logo" height="40px"
+                        width="100px" />
                 </a>
             </div>
 
@@ -62,14 +63,16 @@
                     </a>
                 </div>
                 <div class="nav-item">
+                    <a class="nav-link" href="{{ route('surprise') }}">
+                        <span class="nav-title">轉盤</span>
+                    </a>
+                </div>
+                <div class="nav-item">
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
                         <input type="submit" value="logout">
                     </form>
                 </div>
-                <a class="nav-link" href="{{ route('{name}', ['name' => $user->name ?? 'None']) }}">
-                    <span class="nav-title">轉盤</span>
-                </a>
             </div>
         </div>
 
