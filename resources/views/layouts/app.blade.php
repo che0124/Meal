0288b911ce7da468cab341e11cfefdb5f0f73a64
chewei <joseph7492748@gmail.com>
2024-05-29 04:16:47 +0800

    Modified navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,29 +130,6 @@
                             </div>
                         </a>
                     </div>
-                    <!-- 通知 -->
-                    {{-- <div class="nav-item">
-                    <a href="#" class="nav-link" id="notifyButton">
-                        <div class="nav-link-icon-title">
-                            <div class="nav-link-icon">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M5.84778 7.52342C6.1872 4.38139 8.83969 2 12 2C15.1603 2 17.8128 4.38139 18.1522 7.52342L18.4533 10.3105C18.4832 10.5871 18.4981 10.7255 18.5176 10.8618C18.6507 11.7924 18.9467 12.6922 19.3921 13.52C19.4574 13.6413 19.5275 13.7614 19.6677 14.0018L20.3169 15.1147C21.0299 16.3369 21.3863 16.948 21.1655 17.4222C21.1414 17.4739 21.1129 17.5234 21.0804 17.5703C20.7821 18 20.0746 18 18.6597 18H5.34029C3.92536 18 3.2179 18 2.91957 17.5703C2.88706 17.5234 2.85862 17.4739 2.83455 17.4222C2.61366 16.948 2.97013 16.3369 3.68307 15.1147L4.33228 14.0018C4.47249 13.7614 4.54259 13.6413 4.60787 13.52C5.05327 12.6922 5.3493 11.7924 5.48238 10.8618C5.50188 10.7255 5.51682 10.5871 5.54671 10.3105L5.84778 7.52342Z"
-                                        fill="none" stroke="currentColor" stroke-width="2" />
-                                    <path
-                                        d="M9 18.5C9 18.8283 9.0776 19.1534 9.22836 19.4567C9.37913 19.76 9.6001 20.0356 9.87868 20.2678C10.1573 20.4999 10.488 20.6841 10.8519 20.8097C11.2159 20.9353 11.606 21 12 21C12.394 21 12.7841 20.9353 13.1481 20.8097C13.512 20.6841 13.8427 20.4999 14.1213 20.2678C14.3999 20.0356 14.6209 19.76 14.7716 19.4567C14.9224 19.1534 15 18.8283 15 18.5"
-                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-                                </svg>
-
-                            </div>
-
-                            <div class="nav-link-title">
-                                <span class="nav-title">通知</span>
-                            </div>
-                        </div>
-                    </a>
-                </div> --}}
                     <!-- 個人檔案 -->
                     <div class="nav-item">
                         <a class="nav-link {{ Request::is('users/*') ? 'active' : '' }}"
@@ -183,7 +160,7 @@
                 <!-- 更多 -->
                 <div class="nav-last">
                     <div class="nav-item">
-                        <a href="#" class="nav-link-last" id="menuButton">
+                        <a href="#" class="nav-link" id="menuButton">
                             <div class="nav-link-icon-title">
                                 <div class="nav-link-icon">
                                     <div class="nav-link-icon">
@@ -203,7 +180,7 @@
                                 </div>
                             </div>
                         </a>
-                        <!-- 選單 -->
+                            <!-- 選單 -->
                         <div class="popup-menu" id="popupMenu">
                             <div class="notify-item-container">
                                 <div class="menu-item">
